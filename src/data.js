9372ad62b261881a5c5384fc38c6172455f0e7de
Sivak Hico <strukggg@gmail.com>
2022-12-20 19:23:42 +0100

    nnn

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const projects = [
     title: "React Tracks",
     subtitle: "React and Python",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
     link: "https://reactbootcamp.com",
   },
